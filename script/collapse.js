95fd3676999fef6565e77276be9ac2d74b703201
ruehlef <f.ruehle@northeastern.edu>
2021-08-24 23:36:30 +0200

    first commit

---
+++ script/collapse.js
@@ -0,0 +1,39 @@
+/* code for talks */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+/* expand current year */
+if (coll != null && coll.length > 0) {
+  coll[0].classList.toggle("active")
+  coll[0].nextElementSibling.style.maxHeight = coll[0].nextElementSibling.scrollHeight + "px"
+}
+
+/* code for news */
+coll = document.querySelectorAll(".newsrow, .readMore")
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    /*get surrounding div*/
+    var parentDiv = this;
+    while (parentDiv.nodeName != "DIV") {
+      parentDiv = parentDiv.parentNode
+    }
+    /*get "more"*/
+    parentDiv.getElementsByClassName("readMore")[0].classList.toggle("hidden");
+    if (parentDiv.style.maxHeight){
+      parentDiv.style.maxHeight = null;
+    } else {
+      parentDiv.style.maxHeight = parentDiv.scrollHeight + "px";
+    } 
+  });
+}
