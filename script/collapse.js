99bb71e4b1190c0bd26a75a7e451da5c7f5038be
ruehlef <f.ruehle@northeastern.edu>
2023-08-15 21:30:31 -0400

    Update collapse.js

--- script/collapse.js
+++ script/collapse.js
@@ -77,6 +77,7 @@ for (i = 0; i < coll.length; i++) {
 /* code to make hash work in safari, i.e. jump to correct seminar series */
 window.onload = function() {
   if(location.hash != "") {
-	location.href = location.hash;
+    const element = document.getElementById(location.hash);
+    element.scrollIntoView();
   }
 }
